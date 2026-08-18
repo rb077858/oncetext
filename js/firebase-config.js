fadc032edf33695184c83135d6deb674a87e31d3
rb077858 <165806112+rb077858@users.noreply.github.com>
2026-08-18 14:41:29 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,12 +2,12 @@
 // האתר סטטי לגמרי - הערכים האלה נועדו להיות ציבוריים בצד הלקוח,
 // ההגנה האמיתית על הנתונים היא ב-firestore.rules.
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyCQvsNfeqk78NYWrna-vdFc9Q0a3LiRwkk",
+  authDomain: "oncetext-73040.firebaseapp.com",
+  projectId: "oncetext-73040",
+  storageBucket: "oncetext-73040.firebasestorage.app",
+  messagingSenderId: "988547148900",
+  appId: "1:988547148900:web:a1b2cad05e858feb540371"
 };
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
